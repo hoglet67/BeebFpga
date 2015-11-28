14b38c0e235f4e38c9566079d564e057c8e0b735
David Banks <dave@hoglet.com>
2015-11-28 17:38:03 +0000

    Updated Altera build to use new ICE T65 Memory Structure
    Change-Id: I88fc23364c12b14579a34049b56027a0de70aaaa

--- src/altera/bbc_micro_de1.vhd
+++ src/altera/bbc_micro_de1.vhd
@@ -210,9 +210,9 @@ begin
 
     bbc_micro : entity work.bbc_micro_core
         generic map (
-            IncludeSID         => true,
-            IncludeMusic5000   => true,
-            IncludeICEDebugger => false,
+            IncludeSID         => false,
+            IncludeMusic5000   => false,
+            IncludeICEDebugger => true,
             UseT65Core         => false,
             UseAlanDCore       => true
             )

c988227b0155cf58db3dc57932f3ad1df854bc04
David Banks <dave@hoglet.com>
2015-12-04 14:22:03 +0000

    Updated the Co Pro SPI to a custome SPI Slave allowing 2-cycle reads
    Change-Id: I96294984d4dc250127cf1b2e49bb550a0a38e797

--- src/common/CoProSPI.vhd
+++ src/common/CoProSPI.vhd
@@ -39,39 +39,40 @@ end;
 
 architecture BEHAVIORAL of CoProSPI is
 
-    signal p_rst_b_int   : std_logic;
+    -- SPI state is simply a counter for the 16 clock cycles in
+    -- the SPI transaction
+    signal spi_state     : integer range 0 to 15;
 
+     -- Indicates the current transaction is valid
+     signal valid         : std_logic;
+
+    -- SPI data out shift register (MISO is bit 7 of this)
+    signal spi_shifter : std_logic_vector (7 downto 0);
+
+    -- Event passing from the SPI to Tube clock domain
+    signal tube_go       : std_logic;
+    signal tube_go1      : std_logic;
+    signal tube_go2      : std_logic;
+
+    -- Signals driving the tube chip
+    signal p_rst_b_int   : std_logic;
     signal p_cs_b        : std_logic;
     signal p_rdnw        : std_logic;
     signal p_addr        : std_logic_vector (2 downto 0);
     signal p_data_in     : std_logic_vector (7 downto 0);
     signal p_data_out    : std_logic_vector (7 downto 0);
-    signal p_data_out_r  : std_logic_vector (7 downto 0);
 
-    signal di_req        : std_logic;
-    signal di            : std_logic_vector (7 downto 0);
-    signal wren          : std_logic;
-    signal wr_ack        : std_logic;
-    signal do_valid      : std_logic;
-    signal do            : std_logic_vector (7 downto 0);
+    -- Latched data out of the tube chip
+    signal p_data_out_r : std_logic_vector (7 downto 0);
 
-    signal p_spi_ssel1   : std_logic;
-    signal p_spi_ssel2   : std_logic;
-
-    type SPI_STATE_TYPE is (
+    -- Tube state is pretty simple as well
+    type TUBE_STATE_TYPE is (
         IDLE,
-        WRITE1,
-        WRITE2,
-        WRITE3,
-        READ1,
-        READ2,
-        READ3,
-        READ4,
-        READ5,
-        READ6
+        READ,
+        WRITE
     );
 
-    signal spi_state : SPI_STATE_TYPE := IDLE;
+    signal tube_state : TUBE_STATE_TYPE := IDLE;
 
 begin
 
@@ -104,157 +105,144 @@ begin
     );
     p_rst_b <= p_rst_b_int;
 
-    inst_spi_slave : entity work.spi_slave
-    generic map (
-        N  => 8,                        -- 32bit serial word length is default
-        CPOL => '0',                    -- SPI mode selection (mode 0 default)
-        CPHA => '0',                    -- CPOL = clock polarity, CPHA = clock phase.
-        PREFETCH => 3                   -- prefetch lookahead cycles
-    )
-    port map (
-        clk_i           => p_clk,       -- internal interface clock (clocks di/do registers)
-        spi_ssel_i      => p_spi_ssel,  -- spi bus slave select line
-        spi_sck_i       => p_spi_sck,   -- spi bus sck clock (clocks the shift register core)
-        spi_mosi_i      => p_spi_mosi,  -- spi bus mosi input
-        spi_miso_o      => p_spi_miso,  -- spi bus spi_miso_o output
-        di_req_o        => di_req,      -- preload lookahead data request line
-        di_i            => di,          -- parallel load data in (clocked in on rising edge of clk_i)
-        wren_i          => wren,        -- user data write enable
-        wr_ack_o        => wr_ack,      -- write acknowledge
-        do_valid_o      => do_valid,    -- do_o data valid strobe, valid during one clk_i rising edge.
-        do_o            => do,          -- parallel output (clocked out on falling clk_i)
-        --- debug ports
-        do_transfer_o   => open,
-        wren_o          => open,
-        rx_bit_next_o   => open,
-        state_dbg_o     => open,
-        sh_reg_dbg_o    => open
-    );
-
-
-
 ---------------------------------------------------------------------
--- State Machine Interfacing SPI
+-- State Machine Running from the SPI clock
 ---------------------------------------------------------------------
 
--- This first transmitted by from the Pi would be the addressing byte.
--- The most significant bit would be the Read/ NotWrite bit, the middle
--- 4 bits would be always zero and then the 3 least significant bits
--- would be A2, A1, A0.
--- After the first byte if writing the subsequent bytes sent from the
--- Pi would fill the FIFO.
--- After the first byte if reading the subsequent bytes from the FPGA
--- will be from the FIFO/ Register.
-
--- Master -> Slave:
-
-    -- SPI -> Tube can be a direct connection
-    p_data_in <= do;
-
-    -- Tube -> SPI needs a register as there are cases where the tube removes
-    -- the data quickly
-    di <= p_data_out_r;
-
-    process(p_clk)
+    process(p_spi_sck, p_spi_ssel)
     begin
-
-        if rising_edge(p_clk) then
-            -- generate the write signal
-            if wren = '1' then
-                wren <= '0';
-            else
-                wren <= di_req;
-            end if;
-            -- syncronize the ssel signal
-            p_spi_ssel1 <= p_spi_ssel;
-            p_spi_ssel2 <= p_spi_ssel1;
-            if p_rst_b_int = '0' or p_spi_ssel2 = '1' then
-                spi_state <= IDLE;
-                p_addr <= (others => '1');
-                p_rdnw <= '1';
-                p_cs_b <= '1';
-                wren   <= '0';
-            else
+        if p_spi_ssel = '1' then
+            -- this can only be an asynchronous reset
+            spi_state <= 0;
+        else
+            -- This works in Mode 0 only...
+            -- MOSI should be sampled on the rising edge
+            if rising_edge(p_spi_sck) then
+                -- for convenience, internal state also changes on this edge
+                spi_state <= spi_state + 1;
+                -- capture the important bits from the transaction
                 case spi_state is
-                -- Wait for command, then latch it
-                when IDLE =>
-                    if do_valid = '1' then
-                        if do(6 downto 3) = "0000" then
-                            p_addr <= do(2 downto 0);
-                            if do(7) = '0' then
-                                p_rdnw <= '0';
-                                spi_state <= WRITE1;
-                            else
-                                p_rdnw <= '1';
-                                spi_state <= READ1;
-                            end if;
+                    when 0 =>
+                        -- Ignore commands where B7=0
+                        valid <= p_spi_mosi;
+                    when 1 =>
+                        p_rdnw <= p_spi_mosi;
+                    when 2 =>
+                        p_addr(2) <= p_spi_mosi;
+                    when 3 =>
+                        p_addr(1) <= p_spi_mosi;
+                    when 4 =>
+                        p_addr(0) <= p_spi_mosi;
+                        -- This is the earliest we can ask for a read request
+                        if valid = '1' and p_rdnw = '1' then
+                            -- An edge signifies the read request
+                            tube_go <= not tube_go;
                         end if;
-                    end if;
-
-                -- Process write command
-                when WRITE1 =>
-                    if do_valid = '0' then
-                        spi_state <= WRITE2;
-                    end if;
-                when WRITE2 =>
-                    if do_valid = '1' then
-                        -- assert CS for one cycle
-                        p_cs_b <= '0';
-                        spi_state <= WRITE3;
-                    end if;
-                when WRITE3 =>
-                    p_cs_b <= '1';
-                    if do_valid = '0' then
-                        spi_state <= IDLE;
-                    end if;
-
-                -- Process read command
-                when READ1 =>
-                    -- assert CS for one cycle
-                    p_cs_b <= '0';
-                    spi_state <= READ2;
-                when READ2 =>
-                    -- latch the data read out of the tube chip
-                    p_cs_b <= '1';
-                    p_data_out_r <= p_data_out;
-                    --wren <= '1';
-                    spi_state <= READ3;
-                when READ3 =>
-                    --wren <= '0';
-                    -- wait for second byte of command before returning to idle
-                    if do_valid = '1' then
-                        spi_state <= READ4;
-                    end if;
-                when READ4 =>
-                    -- wait for second byte of command before returning to idle
-                    if do_valid = '0' then
-                        spi_state <= READ5;
-                    end if;
-                when READ5 =>
-                    -- wait for third byte of command before returning to idle
-                    if do_valid = '1' then
-                        spi_state <= READ6;
-                    end if;
-                when READ6 =>
-                    -- wait for third byte of command before returning to idle
-                    if do_valid = '0' then
-                        spi_state <= IDLE;
-                    end if;
-
-                when others =>
-                    spi_state <= IDLE;
+                    when 8 =>
+                        p_data_in(7) <= p_spi_mosi;
+                    when 9 =>
+                        p_data_in(6) <= p_spi_mosi;
+                    when 10 =>
+                        p_data_in(5) <= p_spi_mosi;
+                    when 11 =>
+                        p_data_in(4) <= p_spi_mosi;
+                    when 12 =>
+                        p_data_in(3) <= p_spi_mosi;
+                    when 13 =>
+                        p_data_in(2) <= p_spi_mosi;
+                    when 14 =>
+                        p_data_in(1) <= p_spi_mosi;
+                    when 15 =>
+                        p_data_in(0) <= p_spi_mosi;
+                        -- This is the earliest we can ask for a write request
+                        if valid = '1' and p_rdnw = '0' then
+                            -- An edge signifies the read request
+                            tube_go <= not tube_go;
+                        end if;
+                    when others =>
+                        null;
+                end case;
+            end if;
+            -- This works in Mode 0 only...
+            -- MISO should change on the falling edge
+            if falling_edge(p_spi_sck) then
+                case spi_state is
+                    when 0 =>
+                        spi_shifter <= (others => '0');
+                    when 8 =>
+                        -- is it a valid read cycle?
+                        if valid = '1' and p_rdnw = '1' then
+                            -- load the shift register just in time...
+                            spi_shifter <= p_data_out_r;
+                        else
+                            spi_shifter <= (others => '0');
+                        end if;
+                    when others =>
+                        -- shift the shift register one place to the left
+                        spi_shifter <= spi_shifter(6 downto 0) & '0';
                 end case;
             end if;
         end if;
     end process;
 
-    test(0) <= '1' when spi_state = IDLE   else '0';
-    test(1) <= '1' when spi_state = WRITE1 else '0';
-    test(2) <= '1' when spi_state = WRITE2 else '0';
-    test(3) <= '1' when spi_state = WRITE3 else '0';
-    test(4) <= '1' when spi_state = READ1  else '0';
-    test(5) <= '1' when spi_state = READ2  else '0';
-    test(6) <= '1' when spi_state = READ3  else '0';
-    test(7) <= '1' when spi_state = READ4  else '0';
+    -- MISO is simply the MS bit of the shift regster
+    p_spi_miso <= spi_shifter(7);
+
+---------------------------------------------------------------------
+-- State Machine Running from the tube clock
+---------------------------------------------------------------------
+
+    process(p_clk)
+    begin
+
+        if rising_edge(p_clk) then
+            -- Synchronize the tube go signal
+            tube_go1 <= tube_go;
+            tube_go2 <= tube_go1;
+
+            if p_rst_b_int = '0' then
+                tube_state <= IDLE;
+                p_cs_b <= '1';
+            else
+                case tube_state is
+                when IDLE =>
+                    -- Wait for an edge on tube_go
+                    if tube_go1 /= tube_go2 then
+                        -- assert CS for on the next clock edge
+                        p_cs_b <= '0';
+                        if p_rdnw = '0' then
+                            tube_state <= WRITE;
+                        else
+                            tube_state <= READ;
+                        end if;
+                    else
+                        p_cs_b <= '1';
+                    end if;
+
+                -- Process write command
+                when WRITE =>
+                    -- deassert CS on the next clock edge
+                    p_cs_b <= '1';
+                    -- back to idle
+                    tube_state <= IDLE;
+
+                -- Process read command
+                when READ =>
+                    -- deassert CS on the next clock edge
+                    p_cs_b <= '1';
+                    -- latch the data read out of the tube chip
+                    p_data_out_r <= p_data_out;
+                    -- back to idle
+                    tube_state <= IDLE;
+
+                -- Should never get here
+                when others =>
+                    tube_state <= IDLE;
+                end case;
+            end if;
+        end if;
+    end process;
+
+    test <= std_logic_vector(to_unsigned(spi_state, 4)) & p_cs_b & p_addr;
 
 end BEHAVIORAL;

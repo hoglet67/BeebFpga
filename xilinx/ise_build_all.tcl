918d6ab0958309f8d4849ed81fd4475976f0e067
David Banks <dave@hoglet.com>
2020-03-18 14:58:10 +0000

    Spec Next: refresh project/build scrpits

--- xilinx/ise_build_all.tcl
+++ xilinx/ise_build_all.tcl
@@ -1,7 +1,7 @@
 #!/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xtclsh
-#project open bbc_micro_spec_next.xise
-#process run "Generate Programming File"
-#project close
+project open bbc_micro_spec_next.xise
+process run "Generate Programming File"
+project close
 project open bbc_master_spec_next.xise
 process run "Generate Programming File"
 project close

c4ab78267b427b23bd23fdd499013730c33292cc
David Banks <dave@hoglet.com>
2022-02-12 15:27:32 +0000

    Papilio Duo: updated tcl scripts

--- xilinx/ise_clean_duo.tcl
+++ xilinx/ise_clean_duo.tcl
@@ -1,8 +1,8 @@
 #!/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xtclsh
 project open bbc_micro_duo.xise
-process run "Generate Programming File"
+project clean
 project close
 project open bbc_master_duo.xise
-process run "Generate Programming File"
+project clean
 project close
 exit

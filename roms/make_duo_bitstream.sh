ab820b9fea0541182104218dd124602b3b41c2fc
David Banks <dave@hoglet.com>
2022-02-12 15:21:52 +0000

    Papilio Duo: make_duo_bitstream compiles/merges latest AVR firmware

--- roms/make_duo_bitstream.sh
+++ roms/make_duo_bitstream.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 . /opt/Xilinx/14.7/ISE_DS/settings64.sh
 
-run_data2mem=$false
+# Set to true to run data2mem to merge in latest AVR firmware for ICE T65
+run_data2mem=true
 
+# Papilio Programmer settings
 PAPILIO_LOADER=/opt/GadgetFactory/papilio-loader/programmer
 PROG=${PAPILIO_LOADER}/linux64/papilio-prog
 BSCAN=${PAPILIO_LOADER}/bscan_spi_xc6slx9.bit
-IMAGE=tmp/rom_image.bin
 
-# Build a fresh ROM image
+# Merge in the AVR Firmware
+
+if [ "$run_data2mem" = true ]; then
+
+    echo "Merging AVR Firmware"
+
+    pushd ../AtomBusMon/target/lx9_dave/ice6502
+    make -B avr_progmem.mem
+    popd
+    data2mem -bm ../xilinx/duo_cpumon_modelb_bd.bmm \
+             -bd ../AtomBusMon/target/lx9_dave/ice6502/avr_progmem.mem \
+             -bt ../xilinx/working/bbc_micro_duo/bbc_micro_duo.bit \
+             -o b tmp/bbc_micro_duo.bit
+
+    pushd ../AtomBusMon/target/lx9_dave/ice65c02
+    make -B avr_progmem.mem
+    popd
+    data2mem -bm ../xilinx/duo_cpumon_master_bd.bmm \
+             -bd ../AtomBusMon/target/lx9_dave/ice65c02/avr_progmem.mem \
+             -bt ../xilinx/working/bbc_master_duo/bbc_micro_duo.bit \
+             -o b tmp/bbc_master_duo.bit
+
+else
+
+    echo "Skipped Merging AVR Firmware"
+
+    cp ../xilinx/working/bbc_micro_duo/bbc_micro_duo.bit  tmp/bbc_micro_duo.bit
+    cp ../xilinx/working/bbc_master_duo/bbc_micro_duo.bit tmp/bbc_master_duo.bit
+
+fi
+
+# Build a fresh ROM image (tmp/rom_image.bin)
 ./make_rom_image.sh
 
 # 0x000000 - Boot Loader
@@ -17,29 +49,25 @@ IMAGE=tmp/rom_image.bin
 # 0x100000 - BBC Micro ROMS
 # 0x140000 - BBC Master ROMS
 
-# Strip of the bitstream header of the BBC Micro FPGA
-
-# Run bitmerge to merge in the ROM images
+# Compile the bitmerge tool, which merges addition data files into the main .bit file
 gcc -o tmp/bitmerge bitmerge.c
 
-promgen -b -u 0 ../xilinx/working/bbc_micro_duo/bbc_micro_duo.bit -o tmp/bbc_micro_duo.bin -p bin -w
-promgen -b -u 0 ../xilinx/working/bbc_master_duo/bbc_micro_duo.bit -o tmp/bbc_master_duo.bin -p bin -w
+# Strip of the bitstream header from target .bit files, so they can be treated as raw data
+promgen -b -u 0 tmp/bbc_micro_duo.bit  -o tmp/bbc_micro_duo.bin  -p bin -w
+promgen -b -u 0 tmp/bbc_master_duo.bit -o tmp/bbc_master_duo.bin -p bin -w
 
+# Merge everything together into a single .bit file
 cp ../xilinx/working/duo_boot_loader/duo_boot_loader.bit    tmp/merged.bit
 ./tmp/bitmerge tmp/merged.bit  54000:tmp/bbc_micro_duo.bin  tmp/merged.bit
 ./tmp/bitmerge tmp/merged.bit  A8000:tmp/bbc_master_duo.bin tmp/merged.bit
-./tmp/bitmerge tmp/merged.bit 100000:$IMAGE                 tmp/merged.bit
+./tmp/bitmerge tmp/merged.bit 100000:tmp/rom_image.bin      tmp/merged.bit
 
+# Remove working files
 rm -f tmp/bitmerge
 rm -f tmp/bbc_micro_duo.bin
 rm -f tmp/bbc_master_duo.bin
-
-# Run data2mem to merge in the AVR Firmware
-#if [ $run_data2mem ]; then
-#BMM_FILE=../src/xilinx/CpuMon_bd.bmm
-#${DATA2MEM} -bm ${BMM_FILE} -bd ../AtomBusMon/firmware/avr_progmem.mem -bt tmp/merged.bit -o b tmp/merged2.bit
-#mv tmp/merged2.bit tmp/merged.bit
-#fi
+rm -f tmp/bbc_micro_duo.bit
+rm -f tmp/bbc_master_duo.bit
 
 # Program the Papilo Duo
 sudo ${PROG} -v -f tmp/merged.bit -b ${BSCAN}  -sa -r

f043ec8427f4ef0d0f6a93e2ceb2de6291b2cb01
David Banks <dave@hoglet.com>
2022-05-15 19:15:33 +0100

    Papilio Duo: add seperate master/model b TCL scripts

---
+++ xilinx/ise_build_duo_modelb.tcl
@@ -0,0 +1,8 @@
+#!/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xtclsh
+project open bbc_micro_duo.xise
+project clean
+project close
+project open bbc_micro_duo.xise
+process run "Generate Programming File"
+project close
+exit

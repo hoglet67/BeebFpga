3831ed61a6faf850753f83ee97839da944de0866
David Banks <dave@hoglet.com>
2025-01-24 15:11:41 +0000

    Tang Nano 20K: Make ROM script produces a single combined image

--- roms/make_rom_image_tang9k.sh
+++ roms/make_rom_image_tang9k.sh
@@ -1,36 +1,65 @@
 #!/bin/bash
 
-# Create the 64KB minimal ROM image for the Tang 9K build
-#
-# This contains:
-
-# 16x 16K ROMS images for the Model B
-# 16x 16K ROMS images for the Master 128
+# Create the ROM images for the Tang 9K and 20K builds
 
 MMFS=MMFS
 
 # Beeb ROM Images
 
-IMAGE=tmp/tang_image_beeb_000000.bin
-rm -f $IMAGE
-cat bbcb/os12.rom              >> $IMAGE
-cat bbcb/${MMFS}/M/SWMMFS.rom  >> $IMAGE
-cat bbcb/ram_master_v6.rom     >> $IMAGE
-cat bbcb/basic2.rom            >> $IMAGE
+IMAGE1=tmp/tang_image_beeb_000000.bin
+rm -f $IMAGE1
+cat bbcb/os12.rom              >> $IMAGE1
+cat bbcb/${MMFS}/M/SWMMFS.rom  >> $IMAGE1
+cat bbcb/ram_master_v6.rom     >> $IMAGE1
+cat bbcb/basic2.rom            >> $IMAGE1
 
 # Master ROM Images
 
-IMAGE=tmp/tang_image_master_327680.bin
-rm -f $IMAGE
-cat m128/mos.rom               >> $IMAGE
+IMAGE2=tmp/tang_image_master_327680.bin
+rm -f $IMAGE2
+cat m128/mos.rom               >> $IMAGE2
 
-IMAGE=tmp/tang_image_master_393216.bin
+IMAGE3=tmp/tang_image_master_393216.bin
+rm -f $IMAGE3
+cat m128/dfs.rom               >> $IMAGE3 # Retain this for SRAM Utils
+cat m128/${MMFS}/M/MAMMFS.rom  >> $IMAGE3 # MMFS in a higher slot
+cat m128/viewsht.rom           >> $IMAGE3
+cat m128/edit.rom              >> $IMAGE3
+cat m128/basic4.rom            >> $IMAGE3
+cat m128/adfs.rom              >> $IMAGE3
+cat m128/view.rom              >> $IMAGE3
+cat m128/terminal.rom          >> $IMAGE3
+
+# Concatenate into a single image that can be used on the Tanh Nano 9K or 20K
+#
+# Tang Nano 9K
+#   Beeb loads 4 ROMs from address 0x000000
+#   Master loads 16 ROMs from address 0x040000
+#
+# Tang Nano 20K
+#   Beeb loads 4 ROMs from address 0x500000
+#   Master loads 16 ROMs from address 0x540000
+#
+# The combined ROM image contains:
+#   offset 0x00000 - 0x0FFFF - Miminal 64KB Beeb Image (Beeb ROMs 4, 8, E, F)
+#   offset 0x10000 - 0x3FFFF - Blank (unused Beeb ROMs)
+#   offset 0x40000 - 0x4FFFF - Blank (Master ROMs 0-3)
+#   offset 0x50000 - 0x53FFF - Master MOS (ROM 4)
+#   offset 0x54000 - 0x5FFFF - Blank (ROMs 5-7)
+#   offset 0x60000 - 0x7FFFF - Master ROMs (ROMs 8-15)
+#
+# NB: ROM 4 is the MOS in both Beeb and Master
+
+IMAGE=tmp/tang_image_combined.bin
 rm -f $IMAGE
-cat m128/dfs.rom               >> $IMAGE # Retain this for SRAM Utils
-cat m128/${MMFS}/M/MAMMFS.rom  >> $IMAGE # MMFS in a higher slot
-cat m128/viewsht.rom           >> $IMAGE
-cat m128/edit.rom              >> $IMAGE
-cat m128/basic4.rom            >> $IMAGE
-cat m128/adfs.rom              >> $IMAGE
-cat m128/view.rom              >> $IMAGE
-cat m128/terminal.rom          >> $IMAGE
+
+cat $IMAGE1 >> $IMAGE
+dd if=/dev/zero bs=16384 count=16 >> $IMAGE
+cat $IMAGE2 >> $IMAGE
+dd if=/dev/zero bs=16384 count=3 >> $IMAGE
+cat $IMAGE3 >> $IMAGE
+
+echo $IMAGE
+ls -l $IMAGE
+echo "On a TANG Nano  9K program to external FLASH address 0x000000"
+echo "On a TANG Nano 20K program to external FLASH address 0x500000"

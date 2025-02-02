eaae57dde18547637d84392c65786a8763821232
David Banks <dave@hoglet.com>
2025-02-02 18:29:17 +0000

    Add a script to speed up MMFS testing

---
+++ roms/copy_mmfs_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MMFS=../../MMFS/build
+cp ${MMFS}/MMFS/M/SWMMFS.rom bbcb/MMFS/M/SWMMFS.rom
+cp ${MMFS}/MMFS2/M/SWMMFS.rom bbcb/MMFS2/M/SWMMFS.rom
+cp ${MMFS}/MMFS/M/MAMMFS.rom m128/MMFS/M/MAMMFS.rom
+cp ${MMFS}/MMFS2/M/MAMMFS.rom m128/MMFS2/M/MAMMFS.rom

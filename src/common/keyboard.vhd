bea7f0ecf4525403a3456c7a6d3436c06744b1eb
David Banks <dave@hoglet.com>
2025-05-15 18:37:54 +0100

    Core: Updated keyboard to add Master 128 keypad bindings into cols 10-12

--- src/common/keyboard.vhd
+++ src/common/keyboard.vhd
@@ -391,22 +391,24 @@ begin
             end if;
 
             -- To stop Quartus inferring latches
-            keys(10) <= (others => '0');
-            keys(11) <= (others => '0');
-            keys(12) <= (others => '0');
+            keys(10)(0) <= '0';
+            keys(11)(0) <= '0';
+            keys(12)(0) <= '0';
+            keys(12)(1) <= '0';
+            keys(12)(2) <= '0';
             keys(13) <= (others => '0');
             keys(14) <= (others => '0');
             keys(15) <= (others => '0');
 
             -- Copy DIP switches through to row 0
-            keys(2)(0) <= DIP_SWITCH(7);
-            keys(3)(0) <= DIP_SWITCH(6);
-            keys(4)(0) <= DIP_SWITCH(5);
-            keys(5)(0) <= DIP_SWITCH(4);
-            keys(6)(0) <= DIP_SWITCH(3);
-            keys(7)(0) <= DIP_SWITCH(2);
-            keys(8)(0) <= DIP_SWITCH(1);
-            keys(9)(0) <= DIP_SWITCH(0);
+            keys( 2)(0) <= DIP_SWITCH(7);
+            keys( 3)(0) <= DIP_SWITCH(6);
+            keys( 4)(0) <= DIP_SWITCH(5);
+            keys( 5)(0) <= DIP_SWITCH(4);
+            keys( 6)(0) <= DIP_SWITCH(3);
+            keys( 7)(0) <= DIP_SWITCH(2);
+            keys( 8)(0) <= DIP_SWITCH(1);
+            keys( 9)(0) <= DIP_SWITCH(0);
 
             if keyb_valid = '1' and state = enabled then
 
@@ -424,90 +426,120 @@ begin
                     releasex <= '0';
                     extended <= '0';
 
-                    -- Decode scan codes
-                    case keyb_data is
-                    when X"12" => keys(0)(0) <= not releasex; -- Left SHIFT
-                    when X"59" => keys(0)(0) <= not releasex; -- Right SHIFT
-                    when X"15" => keys(0)(1) <= not releasex; -- Q
-                    when X"09" => keys(0)(2) <= not releasex; -- F10 (F0)
-                    when X"16" => keys(0)(3) <= not releasex; -- 1
-                    when X"58" => keys(0)(4) <= not releasex; -- CAPS LOCK
-                    when X"11" => if extended = '1' then right_alt <= not releasex; else keys(0)(5) <= not releasex; end if; -- LEFT ALT (SHIFT LOCK)
-                    when X"0D" => keys(0)(6) <= not releasex; -- TAB
-                    when X"76" => keys(0)(7) <= not releasex; -- ESCAPE
-                    when X"14" => keys(1)(0) <= not releasex; -- LEFT/RIGHT CTRL (CTRL)
-                    when X"26" => keys(1)(1) <= not releasex; -- 3
-                    when X"1D" => keys(1)(2) <= not releasex; -- W
-                    when X"1E" => keys(1)(3) <= not releasex; -- 2
-                    when X"1C" => keys(1)(4) <= not releasex; -- A
-                    when X"1B" => keys(1)(5) <= not releasex; -- S
-                    when X"1A" => keys(1)(6) <= not releasex; -- Z
-                    when X"05" => keys(1)(7) <= not releasex; -- F1
-                    when X"25" => keys(2)(1) <= not releasex; -- 4
-                    when X"24" => keys(2)(2) <= not releasex; -- E
-                    when X"23" => keys(2)(3) <= not releasex; -- D
-                    when X"22" => keys(2)(4) <= not releasex; -- X
-                    when X"21" => keys(2)(5) <= not releasex; -- C
-                    when X"29" => keys(2)(6) <= not releasex; -- SPACE
-                    when X"06" => keys(2)(7) <= not releasex; -- F2
-                    when X"2E" => keys(3)(1) <= not releasex; -- 5
-                    when X"2C" => keys(3)(2) <= not releasex; -- T
-                    when X"2D" => keys(3)(3) <= not releasex; -- R
-                    when X"2B" => keys(3)(4) <= not releasex; -- F
-                    when X"34" => keys(3)(5) <= not releasex; -- G
-                    when X"2A" => keys(3)(6) <= not releasex; -- V
-                    when X"04" => keys(3)(7) <= not releasex; -- F3
-                    when X"0C" => keys(4)(1) <= not releasex; -- F4
-                    when X"3D" => keys(4)(2) <= not releasex; -- 7
-                    when X"36" => keys(4)(3) <= not releasex; -- 6
-                    when X"35" => keys(4)(4) <= not releasex; -- Y
-                    when X"33" => keys(4)(5) <= not releasex; -- H
-                    when X"32" => keys(4)(6) <= not releasex; -- B
-                    when X"03" => keys(4)(7) <= not releasex; -- F5
-                    when X"3E" => keys(5)(1) <= not releasex; -- 8
-                    when X"43" => keys(5)(2) <= not releasex; -- I
-                    when X"3C" => keys(5)(3) <= not releasex; -- U
-                    when X"3B" => keys(5)(4) <= not releasex; -- J
-                    when X"31" => keys(5)(5) <= not releasex; -- N
-                    when X"3A" => keys(5)(6) <= not releasex; -- M
-                    when X"0B" => keys(5)(7) <= not releasex; -- F6
-                    when X"83" => keys(6)(1) <= not releasex; -- F7
-                    when X"46" => keys(6)(2) <= not releasex; -- 9
-                    when X"44" => keys(6)(3) <= not releasex; -- O
-                    when X"42" => keys(6)(4) <= not releasex; -- K
-                    when X"4B" => keys(6)(5) <= not releasex; -- L
-                    when X"41" => keys(6)(6) <= not releasex; -- ,
-                    when X"0A" => keys(6)(7) <= not releasex; -- F8
-                    when X"4E" => keys(7)(1) <= not releasex; -- -
-                    when X"45" => keys(7)(2) <= not releasex; -- 0
-                    when X"4D" => keys(7)(3) <= not releasex; -- P
-                    when X"0E" => keys(7)(4) <= not releasex; -- ` (@)
-                    when X"4C" => keys(7)(5) <= not releasex; -- ;
-                    when X"49" => keys(7)(6) <= not releasex; -- .
-                    when X"01" => keys(7)(7) <= not releasex; -- F9
-                    when X"55" => keys(8)(1) <= not releasex; -- = (^)
-                    when X"5D" => keys(8)(2) <= not releasex; -- # (_)
-                    when X"54" => keys(8)(3) <= not releasex; -- [
-                    when X"52" => keys(8)(4) <= not releasex; -- '
-                    when X"5B" => keys(8)(5) <= not releasex; -- ]
-                    when X"4A" => keys(8)(6) <= not releasex; -- /
-                    when X"61" => keys(8)(7) <= not releasex; -- \
-                    when X"6B" => keys(9)(1) <= not releasex; -- LEFT
-                    when X"72" => keys(9)(2) <= not releasex; -- DOWN
-                    when X"75" => keys(9)(3) <= not releasex; -- UP
-                    when X"5A" => keys(9)(4) <= not releasex; -- RETURN
-                    when X"66" => keys(9)(5) <= not releasex; -- BACKSPACE (DELETE)
-                    when X"69" => keys(9)(6) <= not releasex; -- END (COPY)
-                    when X"74" => keys(9)(7) <= not releasex; -- RIGHT
+                    if extended = '1' then
 
-                    -- F12 is used for the BREAK key, which in the real BBC asserts
-                    -- reset.  Here we pass this out to the top level which may
-                    -- optionally OR it in to the system reset
-                    when X"07" => BREAK_OUT <= not releasex; -- F12 (BREAK)
+                        -- Decode extended scan codes
+                        case keyb_data is
+                            when X"11" => right_alt   <= not releasex; -- RIGHT ALT (Config)
+                            when X"14" => keys( 1)(0) <= not releasex; -- RIGHT CTRL (CTRL)
+                            when X"69" => keys( 9)(6) <= not releasex; -- END (COPY)
+                            when X"75" => keys( 9)(3) <= not releasex; -- UP
+                            when X"6B" => keys( 9)(1) <= not releasex; -- LEFT
+                            when X"72" => keys( 9)(2) <= not releasex; -- DOWN
+                            when X"74" => keys( 9)(7) <= not releasex; -- RIGHT
+                            when x"4A" => keys(10)(4) <= not releasex; -- Keypad /
+                            when x"5A" => keys(12)(3) <= not releasex; -- Keypad Return
+                            when x"70" => keys(12)(5) <= not releasex; -- INSERT Keypad ,
+                            when x"71" => keys(11)(4) <= not releasex; -- DELETE Keypad Delete
+                        end case;
 
-                    when others => null;
-                    end case;
+                    else
 
+                        -- Decode normal scan codes
+                        case keyb_data is
+                            when X"12" => keys( 0)(0) <= not releasex; -- Left SHIFT
+                            when X"59" => keys( 0)(0) <= not releasex; -- Right SHIFT
+                            when X"15" => keys( 0)(1) <= not releasex; -- Q
+                            when X"09" => keys( 0)(2) <= not releasex; -- F10 (F0)
+                            when X"16" => keys( 0)(3) <= not releasex; -- 1
+                            when X"58" => keys( 0)(4) <= not releasex; -- CAPS LOCK
+                            when X"11" => keys( 0)(5) <= not releasex; -- LEFT ALT (SHIFT LOCK)
+                            when X"0D" => keys( 0)(6) <= not releasex; -- TAB
+                            when X"76" => keys( 0)(7) <= not releasex; -- ESCAPE
+                            when X"14" => keys( 1)(0) <= not releasex; -- LEFT CTRL (CTRL)
+                            when X"26" => keys( 1)(1) <= not releasex; -- 3
+                            when X"1D" => keys( 1)(2) <= not releasex; -- W
+                            when X"1E" => keys( 1)(3) <= not releasex; -- 2
+                            when X"1C" => keys( 1)(4) <= not releasex; -- A
+                            when X"1B" => keys( 1)(5) <= not releasex; -- S
+                            when X"1A" => keys( 1)(6) <= not releasex; -- Z
+                            when X"05" => keys( 1)(7) <= not releasex; -- F1
+                            when X"25" => keys( 2)(1) <= not releasex; -- 4
+                            when X"24" => keys( 2)(2) <= not releasex; -- E
+                            when X"23" => keys( 2)(3) <= not releasex; -- D
+                            when X"22" => keys( 2)(4) <= not releasex; -- X
+                            when X"21" => keys( 2)(5) <= not releasex; -- C
+                            when X"29" => keys( 2)(6) <= not releasex; -- SPACE
+                            when X"06" => keys( 2)(7) <= not releasex; -- F2
+                            when X"2E" => keys( 3)(1) <= not releasex; -- 5
+                            when X"2C" => keys( 3)(2) <= not releasex; -- T
+                            when X"2D" => keys( 3)(3) <= not releasex; -- R
+                            when X"2B" => keys( 3)(4) <= not releasex; -- F
+                            when X"34" => keys( 3)(5) <= not releasex; -- G
+                            when X"2A" => keys( 3)(6) <= not releasex; -- V
+                            when X"04" => keys( 3)(7) <= not releasex; -- F3
+                            when X"0C" => keys( 4)(1) <= not releasex; -- F4
+                            when X"3D" => keys( 4)(2) <= not releasex; -- 7
+                            when X"36" => keys( 4)(3) <= not releasex; -- 6
+                            when X"35" => keys( 4)(4) <= not releasex; -- Y
+                            when X"33" => keys( 4)(5) <= not releasex; -- H
+                            when X"32" => keys( 4)(6) <= not releasex; -- B
+                            when X"03" => keys( 4)(7) <= not releasex; -- F5
+                            when X"3E" => keys( 5)(1) <= not releasex; -- 8
+                            when X"43" => keys( 5)(2) <= not releasex; -- I
+                            when X"3C" => keys( 5)(3) <= not releasex; -- U
+                            when X"3B" => keys( 5)(4) <= not releasex; -- J
+                            when X"31" => keys( 5)(5) <= not releasex; -- N
+                            when X"3A" => keys( 5)(6) <= not releasex; -- M
+                            when X"0B" => keys( 5)(7) <= not releasex; -- F6
+                            when X"83" => keys( 6)(1) <= not releasex; -- F7
+                            when X"46" => keys( 6)(2) <= not releasex; -- 9
+                            when X"44" => keys( 6)(3) <= not releasex; -- O
+                            when X"42" => keys( 6)(4) <= not releasex; -- K
+                            when X"4B" => keys( 6)(5) <= not releasex; -- L
+                            when X"41" => keys( 6)(6) <= not releasex; -- ,
+                            when X"0A" => keys( 6)(7) <= not releasex; -- F8
+                            when X"4E" => keys( 7)(1) <= not releasex; -- -
+                            when X"45" => keys( 7)(2) <= not releasex; -- 0
+                            when X"4D" => keys( 7)(3) <= not releasex; -- P
+                            when X"0E" => keys( 7)(4) <= not releasex; -- ` (@)
+                            when X"4C" => keys( 7)(5) <= not releasex; -- ;
+                            when X"49" => keys( 7)(6) <= not releasex; -- .
+                            when X"01" => keys( 7)(7) <= not releasex; -- F9
+                            when X"55" => keys( 8)(1) <= not releasex; -- = (^)
+                            when X"5D" => keys( 8)(2) <= not releasex; -- # (_)
+                            when X"54" => keys( 8)(3) <= not releasex; -- [
+                            when X"52" => keys( 8)(4) <= not releasex; -- '
+                            when X"5B" => keys( 8)(5) <= not releasex; -- ]
+                            when X"4A" => keys( 8)(6) <= not releasex; -- /
+                            when X"61" => keys( 8)(7) <= not releasex; -- \
+                            when X"5A" => keys( 9)(4) <= not releasex; -- RETURN
+                            when X"66" => keys( 9)(5) <= not releasex; -- BACKSPACE (DELETE)
+                            when x"74" => keys(10)(1) <= not releasex; -- Keypad 6
+                            when x"75" => keys(10)(2) <= not releasex; -- Keypad 8
+                            when x"79" => keys(10)(3) <= not releasex; -- Keypad +
+                            when x"77" => keys(10)(5) <= not releasex; -- Keypad #
+                            when x"70" => keys(10)(6) <= not releasex; -- Keypad 0
+                            when x"6B" => keys(10)(7) <= not releasex; -- Keypad 4
+                            when x"6C" => keys(11)(1) <= not releasex; -- Keypad 7
+                            when x"7D" => keys(11)(2) <= not releasex; -- Keypad 9
+                            when x"7B" => keys(11)(3) <= not releasex; -- Keypad -
+                            when x"7C" => keys(11)(5) <= not releasex; -- Keypad *
+                            when x"69" => keys(11)(6) <= not releasex; -- Keypad 1
+                            when x"73" => keys(11)(7) <= not releasex; -- Keypad 5
+                            when x"71" => keys(12)(4) <= not releasex; -- Keypad .
+                            when x"7A" => keys(12)(6) <= not releasex; -- Keypad 3
+                            when x"72" => keys(12)(7) <= not releasex; -- Keypad 2
+
+                            -- F12 is used for the BREAK key, which in the real BBC asserts
+                            -- reset.  Here we pass this out to the top level which may
+                            -- optionally OR it in to the system reset
+                            when X"07" => BREAK_OUT <= not releasex; -- F12 (BREAK)
+
+                            when others => null;
+                        end case;
+
+                    end if;
                 end if;
             end if;
         end if;

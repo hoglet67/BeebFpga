9b6c18bc8b9951ec1e434eb7c4e60f37ec6c2fb1
David Banks <dave@hoglet.com>
2025-09-30 11:08:42 +0100

    VIA Simulation: Add 6502, RAM and RTW VIA Test Program

--- simulation/via/build.sh
+++ simulation/via/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-OPTS="--ieee=synopsys"
-
+OPTS="--ieee=synopsys -fexplicit"
+ghdl -a ${OPTS} ../../AtomBusMon/src/T6502/T65_Pack.vhd
+ghdl -a ${OPTS} ../../AtomBusMon/src/T6502/T65_ALU.vhd
+ghdl -a ${OPTS} ../../AtomBusMon/src/T6502/T65_MCode.vhd
+ghdl -a ${OPTS} ../../AtomBusMon/src/T6502/T65.vhd
 ghdl -a ${OPTS} ../../src/common/m6522.vhd
 ghdl -a ${OPTS} m6522_tb.vhd
 ghdl -e ${OPTS} m6522_tb
-ghdl -r ${OPTS} m6522_tb --vcd=test.vcd --stop-time=500us
+ghdl -r ${OPTS} m6522_tb --vcd=test.vcd --stop-time=2000us
--- simulation/via/m6522_tb.vhd
+++ simulation/via/m6522_tb.vhd
@@ -9,7 +9,7 @@ end entity;
 architecture rtl of m6522_tb is
 
     signal clock_48         :   std_logic := '0';
-    signal hard_reset_n     :   std_logic := '0';
+    signal hard_reset_n     :   std_logic := '1';
     signal div3_counter     :   unsigned(1 downto 0) := (others => '0');
     signal clken_counter    :   unsigned(3 downto 0) := (others => '0');
     signal mhz16_clken      :   std_logic;
@@ -20,12 +20,24 @@ architecture rtl of m6522_tb is
 -- CPU signals
     signal via_enable       :   std_logic := '0';
     signal cpu_clken        :   std_logic;
-    signal cpu_r_nw         :   std_logic;
     signal cpu_cycle_mask   :   unsigned(1 downto 0) := (others => '0');
-    signal cpu_a            :   std_logic_vector(3 downto 0);
-    signal cpu_d            :   std_logic_vector(7 downto 0);
 
--- System VIA signals
+    signal cpu_mode         :   std_logic_vector(1 downto 0);
+    signal cpu_ready        :   std_logic;
+    signal cpu_abort_n      :   std_logic;
+    signal cpu_irq_n        :   std_logic;
+    signal cpu_nmi_n        :   std_logic;
+    signal cpu_so_n         :   std_logic;
+    signal cpu_r_nw         :   std_logic;
+    signal cpu_sync         :   std_logic;
+    signal cpu_a_int        :   std_logic_vector(23 downto 0);
+    signal cpu_a            :   std_logic_vector(15 downto 0);
+    signal cpu_di           :   std_logic_vector(7 downto 0);
+    signal cpu_do           :   std_logic_vector(7 downto 0);
+
+    -- VIA signals
+    constant VIA_ADDRESS    :   std_logic_vector(11 downto 0) := x"FE6";
+
     signal via_do       :   std_logic_vector(7 downto 0);
     signal via_do_oe_n  :   std_logic;
     signal via_irq_n    :   std_logic;
@@ -46,8 +58,85 @@ architecture rtl of m6522_tb is
     signal via_pb_out   :   std_logic_vector(7 downto 0);
     signal via_pb_oe_n  :   std_logic_vector(7 downto 0);
 
-    signal cpu_irq_n    :   std_logic := '1';
-    signal cpu_sync     :   std_logic := '0';
+    -- RAM signals
+    type ram_type is array (0 to 65535) of std_logic_vector(7 downto 0);
+    shared variable ram : ram_type := (others => x"00");
+    signal ram_do       :   std_logic_vector(7 downto 0);
+    signal ram_address  : integer;
+
+    -- RTW's VIA test program assembled to 4400
+    signal prog : ram_type := (
+        x"78",x"A9",x"EA",x"8D",x"04",x"02",x"A9",x"45",
+        x"8D",x"05",x"02",x"AD",x"4E",x"FE",x"85",x"70",
+        x"A9",x"7F",x"8D",x"4E",x"FE",x"8D",x"6E",x"FE",
+        x"A9",x"C0",x"8D",x"6E",x"FE",x"A9",x"00",x"8D",
+        x"6B",x"FE",x"85",x"71",x"58",x"A2",x"04",x"8E",
+        x"64",x"FE",x"A0",x"00",x"8C",x"65",x"FE",x"B1",
+        x"00",x"A9",x"01",x"CE",x"65",x"FE",x"AD",x"01",
+        x"10",x"E6",x"71",x"8C",x"65",x"FE",x"A1",x"00",
+        x"CE",x"65",x"FE",x"AD",x"02",x"10",x"E6",x"71",
+        x"8C",x"65",x"FE",x"B1",x"00",x"CE",x"65",x"FE",
+        x"AD",x"03",x"10",x"E6",x"71",x"8C",x"65",x"FE",
+        x"AD",x"34",x"12",x"CE",x"65",x"FE",x"AD",x"04",
+        x"10",x"E6",x"71",x"8C",x"65",x"FE",x"A5",x"12",
+        x"CE",x"65",x"FE",x"AD",x"05",x"10",x"E6",x"71",
+        x"8C",x"65",x"FE",x"A9",x"01",x"CE",x"65",x"FE",
+        x"AD",x"06",x"10",x"E6",x"71",x"8C",x"65",x"FE",
+        x"A1",x"00",x"AD",x"34",x"12",x"AD",x"64",x"FE",
+        x"AD",x"07",x"10",x"E6",x"71",x"8C",x"65",x"FE",
+        x"A1",x"00",x"A5",x"12",x"AD",x"64",x"FE",x"AD",
+        x"08",x"10",x"E6",x"71",x"8C",x"65",x"FE",x"A1",
+        x"00",x"A9",x"01",x"AD",x"64",x"FE",x"AD",x"09",
+        x"10",x"E6",x"71",x"8C",x"65",x"FE",x"B1",x"00",
+        x"A9",x"01",x"AD",x"64",x"FE",x"AD",x"0A",x"10",
+        x"E6",x"71",x"8C",x"65",x"FE",x"A1",x"00",x"AD",
+        x"64",x"FE",x"AD",x"0B",x"10",x"E6",x"71",x"8C",
+        x"65",x"FE",x"B1",x"00",x"AD",x"34",x"12",x"2E",
+        x"64",x"FE",x"AD",x"0C",x"10",x"8E",x"64",x"FE",
+        x"E6",x"71",x"8C",x"65",x"FE",x"B1",x"00",x"A5",
+        x"12",x"2E",x"64",x"FE",x"AD",x"0D",x"10",x"8E",
+        x"64",x"FE",x"E6",x"71",x"8C",x"65",x"FE",x"B1",
+        x"00",x"A9",x"01",x"2E",x"64",x"FE",x"AD",x"0E",
+        x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",x"65",
+        x"FE",x"A1",x"00",x"2E",x"64",x"FE",x"AD",x"0F",
+        x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",x"65",
+        x"FE",x"B1",x"00",x"2E",x"64",x"FE",x"AD",x"10",
+        x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",x"65",
+        x"FE",x"AD",x"34",x"12",x"2E",x"64",x"FE",x"AD",
+        x"11",x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",
+        x"65",x"FE",x"A5",x"12",x"2E",x"64",x"FE",x"AD",
+        x"12",x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",
+        x"65",x"FE",x"A9",x"01",x"2E",x"64",x"FE",x"AD",
+        x"13",x"10",x"8E",x"64",x"FE",x"E6",x"71",x"8C",
+        x"65",x"FE",x"A1",x"00",x"B1",x"00",x"AD",x"14",
+        x"10",x"AD",x"14",x"20",x"E6",x"71",x"8C",x"65",
+        x"FE",x"A1",x"00",x"AD",x"34",x"12",x"AD",x"15",
+        x"10",x"AD",x"15",x"20",x"E6",x"71",x"8C",x"65",
+        x"FE",x"A1",x"00",x"A5",x"12",x"AD",x"16",x"10",
+        x"AD",x"16",x"20",x"E6",x"71",x"8C",x"65",x"FE",
+        x"A1",x"00",x"A9",x"01",x"AD",x"17",x"10",x"AD",
+        x"17",x"20",x"E6",x"71",x"8C",x"65",x"FE",x"B1",
+        x"00",x"A9",x"01",x"AD",x"18",x"10",x"AD",x"18",
+        x"20",x"E6",x"71",x"8C",x"65",x"FE",x"A1",x"00",
+        x"AD",x"19",x"10",x"AD",x"19",x"20",x"E6",x"71",
+        x"8C",x"65",x"FE",x"A1",x"00",x"A9",x"01",x"8D",
+        x"65",x"FE",x"AD",x"1A",x"10",x"E6",x"71",x"8C",
+        x"65",x"FE",x"B1",x"00",x"A9",x"01",x"8D",x"65",
+        x"FE",x"AD",x"1B",x"10",x"E6",x"71",x"8C",x"65",
+        x"FE",x"A1",x"00",x"8D",x"65",x"FE",x"AD",x"1C",
+        x"10",x"E6",x"71",x"78",x"A5",x"70",x"8D",x"4E",
+        x"FE",x"A9",x"7F",x"8D",x"6E",x"FE",x"A9",x"93",
+        x"8D",x"04",x"02",x"A9",x"DC",x"8D",x"05",x"02",
+        x"58",x"60",x"86",x"72",x"BA",x"E8",x"E8",x"BD",
+        x"00",x"01",x"85",x"73",x"E8",x"BD",x"00",x"01",
+        x"A6",x"71",x"9D",x"00",x"43",x"A5",x"73",x"9D",
+        x"00",x"40",x"AD",x"6D",x"FE",x"9D",x"00",x"41",
+        x"AD",x"64",x"FE",x"9D",x"00",x"42",x"A6",x"72",
+        x"A5",x"FC",x"40",x"00",x"00",x"00",x"00",x"00",
+        x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",
+        others => x"00"
+        );
+
 begin
 
     -- Generate the main 48MHz clock
@@ -59,110 +148,216 @@ begin
 
     p_main : process
 
-        procedure VIA_READ(
-            addr : in std_logic_vector(3 downto 0)) is
-        begin
-            cpu_a <= addr;
-            cpu_r_nw <= '1';
-            via_enable <= '1';
-            wait until rising_edge(cpu_clken);
-            cpu_a <= x"0";
-            via_enable <= '0';
-        end procedure;
+        --   procedure VIA_READ(
+      --       addr : in std_logic_vector(3 downto 0)) is
+        --   begin
+      --       cpu_a <= x"fe6" & addr;
+        --       cpu_r_nw <= '1';
+      --       via_enable <= '1';
+        --       wait until rising_edge(cpu_clken);
+      --       cpu_a <= (others => '0');
+        --       via_enable <= '0';
+      --   end procedure;
 
-        procedure VIA_WRITE(
-            addr : in std_logic_vector(3 downto 0);
-            data : in std_logic_vector(7 downto 0)) is
-        begin
-            cpu_d <= data;
-            cpu_a <= addr;
-            cpu_r_nw <= '0';
-            via_enable <= '1';
-            wait until rising_edge(cpu_clken);
-            cpu_d <= x"00";
-            cpu_a <= x"0";
-            cpu_r_nw <= '1';
-            via_enable <= '0';
-        end procedure;
+      --   procedure VIA_WRITE(
+        --       addr : in std_logic_vector(3 downto 0);
+      --       data : in std_logic_vector(7 downto 0)) is
+        --   begin
+      --       cpu_d <= data;
+        --       cpu_a <= x"fe6" & addr;
+      --       cpu_r_nw <= '0';
+        --       via_enable <= '1';
+      --       wait until rising_edge(cpu_clken);
+        --       cpu_d <= x"00";
+      --       cpu_a <= (others => '0');
+        --       cpu_r_nw <= '1';
+      --       via_enable <= '0';
+        --   end procedure;
 
-      procedure VIA_TEST(
-          cycles : in integer) is
-      begin
-          -- STA &FE65
-          cpu_sync <= '1';
-          wait until rising_edge(cpu_clken);
-          cpu_sync <= '0';
-          wait until rising_edge(cpu_clken);
-          wait until rising_edge(cpu_clken);
-          cpu_irq_n <= via_irq_n;
-          VIA_WRITE(x"5", x"00");
-          -- DELAY
-          for i in 1 to cycles loop
-              cpu_irq_n <= via_irq_n;
-              wait until rising_edge(cpu_clken);
-          end loop;
-          -- LDA &FE64
-          cpu_sync <= '1';
-          wait until rising_edge(cpu_clken);
-          cpu_sync <= '0';
-          wait until rising_edge(cpu_clken);
-          wait until rising_edge(cpu_clken);
-          cpu_irq_n <= via_irq_n;
-          VIA_READ(x"4");
-          -- LDA ABS
-          cpu_sync <= '1';
-          wait until rising_edge(cpu_clken);
-          cpu_sync <= '0';
-          wait until rising_edge(cpu_clken);
-          wait until rising_edge(cpu_clken);
-          cpu_irq_n <= via_irq_n;
-          wait until rising_edge(cpu_clken);
-      end procedure;
-
-
+        -- procedure VIA_TEST(
+      --     cycles : in integer) is
+        -- begin
+      --     -- STA &FE65
+        --     cpu_sync <= '1';
+      --     wait until rising_edge(cpu_clken);
+        --     cpu_sync <= '0';
+      --     wait until rising_edge(cpu_clken);
+        --     wait until rising_edge(cpu_clken);
+      --     cpu_irq_n <= via_irq_n;
+        --     VIA_WRITE(x"5", x"00");
+      --     -- DELAY
+        --     for i in 1 to cycles loop
+      --         cpu_irq_n <= via_irq_n;
+        --         wait until rising_edge(cpu_clken);
+      --     end loop;
+        --     -- LDA &FE64
+      --     cpu_sync <= '1';
+        --     wait until rising_edge(cpu_clken);
+      --     cpu_sync <= '0';
+        --     wait until rising_edge(cpu_clken);
+      --     wait until rising_edge(cpu_clken);
+        --     cpu_irq_n <= via_irq_n;
+      --     VIA_READ(x"4");
+        --     -- LDA ABS
+      --     cpu_sync <= '1';
+        --     wait until rising_edge(cpu_clken);
+      --     cpu_sync <= '0';
+        --     wait until rising_edge(cpu_clken);
+      --     wait until rising_edge(cpu_clken);
+        --     cpu_irq_n <= via_irq_n;
+      --     wait until rising_edge(cpu_clken);
+        -- end procedure;
 
     begin
-        wait until rising_edge(cpu_clken);
-        hard_reset_n <= '0';
-        wait until rising_edge(cpu_clken);
+        -- Initialize RAM
+        for i in 0 to 1023 loop
+            -- program
+            ram(16#4400# + i) := prog(i);
+        end loop;
+
+        -- vectors (same as BBC)
+        ram(16#FFFA#) := x"00";
+        ram(16#FFFB#) := x"0D";
+        ram(16#FFFC#) := x"CD";
+        ram(16#FFFD#) := x"D9";
+        ram(16#FFFE#) := x"1C";
+        ram(16#FFFF#) := x"DC";
+
+        -- IRQ handler (same as BBC, no BRK handler)
+        -- DC1C : 85 FC    : STA $FC
+        -- DC1E : 68       : PLA
+        -- DC1F : 48       : PHA
+        -- DC20 : 29 10    : AND #$10
+        -- DC22 : D0 03    : BNE $DC27
+        -- DC24 : 6C 04 02 : JMP ($0204)
+        ram(16#DC1C#) := x"85";
+        ram(16#DC1D#) := x"FC";
+        ram(16#DC1E#) := x"68";
+        ram(16#DC1F#) := x"48";
+        ram(16#DC20#) := x"29";
+        ram(16#DC21#) := x"10";
+        ram(16#DC22#) := x"D0";
+        ram(16#DC23#) := x"03";
+        ram(16#DC24#) := x"6C";
+        ram(16#DC25#) := x"04";
+        ram(16#DC26#) := x"02";
+
+        -- OS
+        -- D9CD : A9 40    : LDA #$40
+        -- D9CF : 8D 00 0D : STA $0D00
+        -- D9D2 : 78       : SEI
+        -- D9D3 : D8       : CLD
+        -- D9D4 : A2 FF    : LDX #$FF
+        -- D9D6 : 9A       : TXS
+        -- D9D7 : 20 00 40 : JSR $4400
+        -- D9DA : 4C DA D9 : JMP $D9DA
+        ram(16#D9CD#) := x"A9";
+        ram(16#D9CE#) := x"40";
+        ram(16#D9CF#) := x"8D";
+        ram(16#D9D0#) := x"00";
+        ram(16#D9D1#) := x"D0";
+        ram(16#D9D2#) := x"78";
+        ram(16#D9D3#) := x"D8";
+        ram(16#D9D4#) := x"A2";
+        ram(16#D9D5#) := x"FF";
+        ram(16#D9D6#) := x"9A";
+        ram(16#D9D7#) := x"20";
+        ram(16#D9D8#) := x"00";
+        ram(16#D9D9#) := x"44";
+        ram(16#D9DA#) := x"4C";
+        ram(16#D9DB#) := x"DA";
+        ram(16#D9DC#) := x"D9";
+
         hard_reset_n <= '1';
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        VIA_WRITE(x"B", x"00");
-        wait until rising_edge(cpu_clken);
-        VIA_WRITE(x"E", x"7F");
-        wait until rising_edge(cpu_clken);
-        VIA_WRITE(x"E", x"C0");
-        wait until rising_edge(cpu_clken);
-        VIA_WRITE(x"4", x"04");
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        wait until rising_edge(cpu_clken);
-        VIA_TEST(15);
-        VIA_TEST(14);
-        VIA_TEST(13);
-        VIA_TEST(12);
-        VIA_TEST(11);
-        VIA_TEST(10);
-        VIA_TEST( 9);
-        VIA_TEST( 8);
-        VIA_TEST( 7);
-        VIA_TEST( 6);
-        VIA_TEST( 5);
-        VIA_TEST( 4);
-        VIA_TEST( 3);
-        VIA_TEST( 2);
+
+        for i in 1 to 10 loop
+            wait until rising_edge(cpu_clken);
+        end loop;
+
+        hard_reset_n <= '0';
+
+        for i in 1 to 10 loop
+            wait until rising_edge(cpu_clken);
+        end loop;
+        hard_reset_n <= '1';
+
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- -- VIA_WRITE(x"B", x"00");
+        -- wait until rising_edge(cpu_clken);
+        -- VIA_WRITE(x"E", x"7F");
+        -- wait until rising_edge(cpu_clken);
+        -- VIA_WRITE(x"E", x"C0");
+        -- wait until rising_edge(cpu_clken);
+        -- VIA_WRITE(x"4", x"04");
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- wait until rising_edge(cpu_clken);
+        -- VIA_TEST(15);
+        -- VIA_TEST(14);
+        -- VIA_TEST(13);
+        -- VIA_TEST(12);
+        -- VIA_TEST(11);
+        -- VIA_TEST(10);
+        -- VIA_TEST( 9);
+        -- VIA_TEST( 8);
+        -- VIA_TEST( 7);
+        -- VIA_TEST( 6);
+        -- VIA_TEST( 5);
+        -- VIA_TEST( 4);
+        -- VIA_TEST( 3);
+        -- VIA_TEST( 2);
         wait;
     end process;
 
-    system_via : entity work.m6522
+    ram_address <= to_integer(unsigned(cpu_a));
+    process(clock_48)
+    begin
+        if rising_edge(clock_48) then
+            if cpu_r_nw = '0' then
+                ram(ram_address) := cpu_do;
+            end if;
+            ram_do <= ram(ram_address);
+        end if;
+    end process;
+
+    core : entity work.T65
         port map (
-            I_RS        => cpu_a,
-            I_DATA      => cpu_d,
+            Mode    => cpu_mode,
+            Res_n   => hard_reset_n,
+            Enable  => cpu_clken,
+            Clk     => clock_48,
+            Rdy     => cpu_ready,
+            Abort_n => cpu_abort_n,
+            IRQ_n   => cpu_irq_n,
+            NMI_n   => cpu_nmi_n,
+            SO_n    => cpu_so_n,
+            R_W_n   => cpu_r_nw,
+            Sync    => cpu_sync,
+            A       => cpu_a_int,
+            DI      => cpu_di,
+            DO      => cpu_do
+            );
+    cpu_a <= cpu_a_int(15 downto 0);
+    cpu_mode <= "00"; -- 6502
+    cpu_ready <= '1';
+    cpu_abort_n <= '1';
+    cpu_irq_n <= via_irq_n;
+    cpu_nmi_n <= '1';
+    cpu_so_n <= '1';
+
+    via_enable <= '1' when cpu_a(15 downto 4) = VIA_ADDRESS else '0';
+
+    cpu_di <= via_do when via_enable = '1' else ram_do;
+
+    via : entity work.m6522
+        port map (
+            I_RS        => cpu_a(3 downto 0),
+            I_DATA      => cpu_do,
             O_DATA      => via_do,
             O_DATA_OE_L => via_do_oe_n,
             I_RW_L      => cpu_r_nw,
@@ -186,13 +381,14 @@ begin
             O_PB        => via_pb_out,
             O_PB_OE_L   => via_pb_oe_n,
             I_P2_H      => via_p2_h,
-            RESET_L     => hard_reset_n, -- System VIA is reset by power on reset only
+            RESET_L     => hard_reset_n, -- VIA is reset by power on reset only
             ENA_4       => via_ena_4,
             CLK         => clock_48
-        );
+            );
 
 
-process(clock_48)
+
+    process(clock_48)
     begin
         if rising_edge(clock_48) then
 
@@ -263,5 +459,6 @@ process(clock_48)
 
         end if;
     end process;
+
 
 end rtl;
